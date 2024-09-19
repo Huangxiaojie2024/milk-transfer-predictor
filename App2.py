64a47fa20d00e914a359df05417212bfb3c7d257
Huangxiaojie2024 <huangxj46@mail3.sysu.edu.cn>
2024-09-19 17:14:40 +0800

    Update App2.py

--- App2.py
+++ App2.py
@@ -43,7 +43,7 @@ if uploaded_file is not None:
 
         for i in range(len(data)):
             fig = plt.figure()
-            shap.plots.force(explainer.expected_value[0], shap_values, matplotlib=True, ax=fig)
+            shap.plots.force(explainer.expected_value[0], shap_values[i], matplotlib=True, ax=fig)
             plt.savefig(f'shap_force_plot_{i}.png')  # 保存每个实例的SHAP force plot
             st.image(f'shap_force_plot_{i}.png')  # 显示SHAP force plot
             plt.clf()  # 清理当前图像
